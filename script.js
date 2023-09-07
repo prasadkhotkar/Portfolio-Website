b7e13d11ee653bf40fc286672425d49c14b5d665
prasad khotkar <109893694+prasadkhotkar@users.noreply.github.com>
2023-09-07 18:49:15 +0530

    bootstrap cdn and js file added

---
+++ script.js
@@ -0,0 +1,8 @@
+let checkButton=document.getElementById("check-btn");
+
+checkButton.addEventListener("click",()=>
+{
+    document.body.style.backgroundColor="black";
+    document.body.style.color="white";
+
+})
